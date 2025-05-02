19bbdb8a0a07fefff24492cc24386a9e3c539879
KhanhaKP <it-khanha.prasatya@traveloka.com>
2025-05-02 10:19:14 +0700

    Update Drive.sh

--- Drive.sh
+++ Drive.sh
@@ -38,7 +38,7 @@ def read_current_version_csv():
     if not os.path.exists(filename):
         with open(filename, 'w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(["Software", "Mungki Version", "Web Version"])
+            writer.writerow(["Software", "Munki Version", "Web Version"])
             writer.writerow(["Google Drive", "0.0.0", ""])  # Default Google Drive version
     
     # Baca file CSV
@@ -46,7 +46,7 @@ def read_current_version_csv():
         reader = csv.DictReader(file)
         versions = {}
         for row in reader:
-            versions[row['Software']] = (row['Mungki Version'], row['Web Version'])
+            versions[row['Software']] = (row['Munki Version'], row['Web Version'])
     
     return versions
 
@@ -67,19 +67,19 @@ def update_web_version_csv(software_name, new_version):
     
     # Tulis kembali file CSV dengan pembaruan
     with open(filename, 'w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=["Software", "Mungki Version", "Web Version"])
+        writer = csv.DictWriter(file, fieldnames=["Software", "Munki Version", "Web Version"])
         writer.writeheader()
         writer.writerows(rows)
 
 # Fungsi untuk membandingkan versi
-def compare_versions(mungki_version, web_version):
-    return mungki_version != web_version
+def compare_versions(Munki_version, web_version):
+    return Munki_version != web_version
 
 # Fungsi untuk mengirim notifikasi ke Telegram
-def send_notification_telegram(software_name, mungki_version, web_version):
+def send_notification_telegram(software_name, Munki_version, web_version):
     telegram_token = "8184924708:AAGZ56uxf7LzbukNx2tdx-F148-9NtLdhOM"  # Ganti dengan token bot Telegram kamu
     chat_id = "-4523501737"  # Ganti dengan chat ID yang sesuai
-    telegram_message = f"Update Available for {software_name}!\nMungki version: {mungki_version}\nLatest version: {web_version}"
+    telegram_message = f"Update Available for {software_name}!\nMunki version: {Munki_version}\nLatest version: {web_version}"
     
     send_text_url = f"https://api.telegram.org/bot{telegram_token}/sendMessage"
     params = {
@@ -118,7 +118,7 @@ def main():
         update_web_version_csv("Google Drive", latest_google_drive_version)
         
         # Kirim notifikasi ke Telegram
-        send_notification_telegram("Google Drive", google_drive_local_version, latest_google_drive_version)
+        #send_notification_telegram("Google Drive", google_drive_local_version, latest_google_drive_version)
     else:
         print("Versi Google Drive sudah yang terbaru.")
 
