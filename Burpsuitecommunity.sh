b2ced2ceb56bd2ee5d3e9dceaf3d32522cb9c9c1
KhanhaKP <it-khanha.prasatya@traveloka.com>
2025-05-27 13:21:34 +0700

    Update Burpsuitecommunity.sh

--- Burpsuitecommunity.sh
+++ Burpsuitecommunity.sh
@@ -4,22 +4,17 @@ import csv
 import os
 import subprocess
 
-# Fungsi untuk mendapatkan versi terbaru Burp Suite Community Edition dari halaman HTML
+# Fungsi untuk mendapatkan versi terbaru Burp Suite Community Edition
 def check_latest_version_burp_suite():
     url = "https://portswigger.net/burp/releases/community/latest"
     response = requests.get(url)
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-
-        # Ambil elemen <h1> dengan id="title" dan class="heading-2"
         version_element = soup.find('h1', {'id': 'title', 'class': 'heading-2'})
-        
         if version_element:
-            # Ambil bagian versi dari teks
             version_text = version_element.get_text(strip=True)
-            latest_version = version_text.split(' ')[-1]  # Ambil bagian terakhir yang merupakan nomor versi
-            #print(f"Latest Burp Suite version found: {latest_version}")
+            latest_version = version_text.split(' ')[-1]
             return latest_version
         else:
             print("Version information not found.")
@@ -28,144 +23,106 @@ def check_latest_version_burp_suite():
         print(f"Gagal mengakses halaman. Status code: {response.status_code}")
         return None
 
-# Fungsi untuk membaca versi dari file CSV
+# Fungsi untuk membaca versi dari CSV
 def read_current_version_csv():
     filename = 'current_version.csv'
-    
-    # Jika file tidak ada, buat file baru dengan nilai default
     if not os.path.exists(filename):
         with open(filename, 'w', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(["Software", "Munki Version", "Web Version"])
-            writer.writerow(["Burp Suite Community", "2023.7.0", ""])  # Default Burp Suite version
-    
-    # Baca file CSV
+            writer.writerow(["Burp Suite Community", "2023.7.0", ""])
     with open(filename, 'r') as file:
         reader = csv.DictReader(file)
-        versions = {}
-        for row in reader:
-            versions[row['Software']] = (row['Munki Version'], row['Web Version'])
-    
-    return versions
+        return {row['Software']: (row['Munki Version'], row['Web Version']) for row in reader}
 
-# Fungsi untuk memperbarui kolom Web Version di file CSV
+# Fungsi update Web Version di CSV
 def update_web_version_csv(software_name, new_version):
     filename = 'current_version.csv'
-    
-    rows = []
-    # Baca semua baris dari CSV
     with open(filename, 'r') as file:
-        reader = csv.DictReader(file)
-        rows = list(reader)
-    
-    # Update versi Web Version pada baris yang sesuai
+        rows = list(csv.DictReader(file))
     for row in rows:
         if row['Software'] == software_name:
             row['Web Version'] = new_version
-    
-    # Tulis kembali file CSV dengan pembaruan
     with open(filename, 'w', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=["Software", "Munki Version", "Web Version"])
         writer.writeheader()
         writer.writerows(rows)
 
-# Fungsi untuk memperbarui kolom Munki Version di file CSV
+# Fungsi update Munki Version di CSV
 def update_munki_version_csv(software_name, new_version):
     filename = 'current_version.csv'
-    rows = []
-    # Baca semua baris dari CSV
     with open(filename, 'r') as file:
-        reader = csv.DictReader(file)
-        rows = list(reader)
-    
-    # Update versi Munki Version pada baris yang sesuai
+        rows = list(csv.DictReader(file))
     for row in rows:
         if row['Software'] == software_name:
             row['Munki Version'] = new_version
-    
-    # Tulis kembali file CSV dengan pembaruan
     with open(filename, 'w', newline='') as file:
         writer = csv.DictWriter(file, fieldnames=["Software", "Munki Version", "Web Version"])
         writer.writeheader()
         writer.writerows(rows)
 
-# Fungsi untuk membandingkan versi
+# Fungsi membandingkan versi
 def compare_versions(local_version, web_version):
     return local_version != web_version
 
-# Jalankan autopkg untuk download dan import ke Munki
+# Jalankan autopkg
 def run_autopkg():
     success = True
     failed_archs = []
-
     try:
-        # Jalankan untuk Apple Silicon (arm64)
         subprocess.run(["autopkg", "run", "com.github.munki-tvlk.munki.BurpSuiteProfessional"], check=True)
         print("Autopkg berhasil dijalankan untuk MAC.")
     except subprocess.CalledProcessError:
         print("Autopkg gagal.")
         success = False
         failed_archs.append("Apple Silicon (arm64)")
-    
     try:
-        # Jalankan untuk Intel (x86_64)
         subprocess.run(["autopkg", "run", "com.github.munki-tvlk.munki.Burp Suite Professionalx86"], check=True)
         print("Autopkg berhasil dijalankan untuk Intel (x86_64).")
     except subprocess.CalledProcessError:
         print("Autopkg gagal untuk Intel (x86_64).")
         success = False
         failed_archs.append("Intel (x86_64)")
-
-    # Kirim notifikasi jika ada yang gagal
     if not success:
         failed_msg = "\n".join(failed_archs)
-        send_notification_telegram("Burp Suite Community", "Failed Import", f"Autopkg gagal untuk:\n{failed_msg}")
-
+        send_notification_lark("Burp Suite Community", "Failed Import", f"Autopkg gagal untuk:\n{failed_msg}")
     return success
 
-# Fungsi untuk mengirim notifikasi ke Telegram
-def send_notification_telegram(software_name, local_version, web_version):
-    telegram_token = "8184924708:AAGZ56uxf7LzbukNx2tdx-F148-9NtLdhOM"   # Ganti dengan token bot Telegram kamu
-    chat_id = "-4523501737"  # Ganti dengan chat ID yang sesuai
-    telegram_message = (f"Update Available for {software_name}!\n"
-                        f"Munki version: {local_version}\n"
-                        f"Latest version: {web_version}\n"
-                        f"Burp Suite Community is Already Import to MunkiAdmin")  
-    send_text_url = f"https://api.telegram.org/bot{telegram_token}/sendMessage"
-    params = {
-        'chat_id': chat_id,
-        'text': telegram_message,
-        'parse_mode': 'Markdown'  # Optional: Menggunakan Markdown untuk format pesan yang lebih baik
+# Fungsi mengirim notifikasi ke Lark
+def send_notification_lark(software_name, local_version, web_version):
+    lark_webhook_url = "https://open.larksuite.com/open-apis/bot/v2/hook/f5a3af1a-bd6a-4482-bf93-fdf9b58bfab6"  # Ganti dengan webhook URL kamu
+    headers = {"Content-Type": "application/json"}
+    message = {
+        "msg_type": "text",
+        "content": {
+            "text": (
+                f"🚨 Update Available: {software_name}!\n"
+                f"Munki Version: {local_version}\n"
+                f"New Version  : {web_version}\n"
+                f"✅ {software_name} has been imported into MunkiAdmin."
+            )
+        }
     }
-    
     try:
-        response = requests.get(send_text_url, params=params)
-        # Cek jika respon status bukan 200, artinya ada masalah
+        response = requests.post(lark_webhook_url, json=message, headers=headers)
         if response.status_code != 200:
-            raise ValueError(f"Request to Telegram returned an error {response.status_code}, response: {response.text}")
+            raise ValueError(f"Lark webhook error {response.status_code}, response: {response.text}")
     except Exception as e:
-        print(f"Error sending notification to Telegram: {e}")
+        print(f"Error sending notification to Lark: {e}")
 
-# Proses utama untuk mengecek versi dan memperbarui jika diperlukan
+# Proses utama
 def main():
-    # Baca semua versi saat ini dari file CSV
     versions = read_current_version_csv()
-    # Cek versi terbaru dari Burp Suite Community Edition
-    latest_burp_suite_version = check_latest_version_burp_suite()
-    burp_suite_local_version, burp_suite_web_version = versions.get('Burp Suite Community', (None, None))
-    # Jika versi web dari website berbeda dengan yang ada di file CSV, perbarui dan kirim notifikasi
-    if compare_versions(burp_suite_local_version, latest_burp_suite_version):
-        print(f"Version baru Burp Suite Community tersedia: {latest_burp_suite_version}")
-        # Perbarui kolom Web Version di file CSV
-        update_web_version_csv("Burp Suite Community", latest_burp_suite_version)
-        # Jalankan autopkg
+    latest_version = check_latest_version_burp_suite()
+    local_version, web_version = versions.get('Burp Suite Community', (None, None))
+    if compare_versions(local_version, latest_version):
+        print(f"New version of Burp Suite Community is available: {latest_version}")
+        update_web_version_csv("Burp Suite Community", latest_version)
         run_autopkg()
-        # Perbarui kolom Munki Version di file CSV
-        update_munki_version_csv("Burp Suite Community", latest_burp_suite_version)
-        # Kirim notifikasi ke Telegram
-        send_notification_telegram("Burp Suite Community", burp_suite_local_version, latest_burp_suite_version)
+        update_munki_version_csv("Burp Suite Community", latest_version)
+        send_notification_lark("Burp Suite Community", local_version, latest_version)
     else:
-        print("Version Burp Suite Community sudah yang terbaru.")
+        print("The version of Burp Suite Community is already up to date.")
 
 if __name__ == "__main__":
     main()
