66ce8cd0e96d6d2aa739eb8aae3b435570e90890
KhanhaKP <it-khanha.prasatya@traveloka.com>
2025-05-27 13:23:33 +0700

    Update Vysor.sh

--- Vysor.sh
+++ Vysor.sh
@@ -4,7 +4,7 @@ import os
 from bs4 import BeautifulSoup
 
 # Token GitHub dan Telegram diambil langsung dari script
-Gtoken = "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"
+Gtoken = "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"
 telegram_token = "8184924708:AAGZ56uxf7LzbukNx2tdx-F148-9NtLdhOM"
 chat_id = "-4523501737"  # Chat ID untuk Telegram
 
@@ -33,14 +33,14 @@ def read_current_version_csv():
     if not os.path.exists(filename):
         with open(filename, 'w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(["Software", "Mungki Version", "Web Version"])
+            writer.writerow(["Software", "Munki Version", "Web Version"])
             writer.writerow(["Vysor", "0.0.0", ""])  # Nilai default jika belum ada data
     
     with open(filename, 'r') as file:
         reader = csv.DictReader(file)
         versions = {}
         for row in reader:
-            versions[row['Software']] = (row['Mungki Version'], row['Web Version'])
+            versions[row['Software']] = (row['Munki Version'], row['Web Version'])
     
     return versions
 
@@ -58,18 +58,18 @@ def update_web_version_csv(software_name, new_version):
             row['Web Version'] = new_version
     
     with open(filename, 'w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=["Software", "Mungki Version", "Web Version"])
+        writer = csv.DictWriter(file, fieldnames=["Software", "Munki Version", "Web Version"])
         writer.writeheader()
         writer.writerows(rows)
 
 # Fungsi untuk membandingkan versi
-def compare_versions(mungki_version, latest_version):
-    return mungki_version != latest_version
+def compare_versions(Munki_version, latest_version):
+    return Munki_version != latest_version
 
 # Fungsi untuk mengirim notifikasi ke Telegram
-def send_notification_telegram(software_name, mungki_version, latest_version):
+def send_notification_telegram(software_name, Munki_version, latest_version):
     telegram_message = (f"Update Available for {software_name}!\n"
-                        f"Mungki version: {mungki_version}\n"
+                        f"Munki version: {Munki_version}\n"
                         f"Latest version: {latest_version}")
     
     send_text_url = f"https://api.telegram.org/bot{telegram_token}/sendMessage"
@@ -97,13 +97,13 @@ def main():
     vysor_current_version, vysor_web_version = versions.get('Vysor', (None, None))
 
     if compare_versions(vysor_current_version, latest_vysor_version):
-        print(f"New version of Vysor available: {latest_vysor_version}")
+        print(f"Version baru Vysor tersedia: {latest_vysor_version}")
         
         update_web_version_csv("Vysor", latest_vysor_version)
         
         send_notification_telegram("Vysor", vysor_current_version, latest_vysor_version)
     else:
-        print("Versi Vysor sudah yang terbaru.")
+        print("Version Vysor sudah yang terbaru.")
 
 if __name__ == "__main__":
     main()
