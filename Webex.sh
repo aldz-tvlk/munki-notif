2d70e911c472d0002311dc6a0c646822430e7f15
KhanhaKP <it-khanha.prasatya@traveloka.com>
2025-05-27 13:23:35 +0700

    Update Webex.sh

--- Webex.sh
+++ Webex.sh
@@ -33,14 +33,14 @@ def read_current_version_csv():
     if not os.path.exists(filename):
         with open(filename, 'w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(["Software", "Mungki Version", "Web Version"])
+            writer.writerow(["Software", "Munki Version", "Web Version"])
             writer.writerow(["Webex Mac", "44.9.0", ""])  # Ganti dengan versi yang sesuai jika perlu
     
     with open(filename, 'r') as file:
         reader = csv.DictReader(file)
         versions = {}
         for row in reader:
-            versions[row['Software']] = (row['Mungki Version'], row['Web Version'])
+            versions[row['Software']] = (row['Munki Version'], row['Web Version'])
     
     return versions
 
@@ -58,23 +58,23 @@ def update_web_version_csv(software_name, new_version):
             row['Web Version'] = new_version
     
     with open(filename, 'w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=["Software", "Mungki Version", "Web Version"])
+        writer = csv.DictWriter(file, fieldnames=["Software", "Munki Version", "Web Version"])
         writer.writeheader()
         writer.writerows(rows)
 
 # Fungsi untuk membandingkan versi
-def compare_versions(mungki_version, web_version):
-    return mungki_version != web_version
+def compare_versions(Munki_version, web_version):
+    return Munki_version != web_version
 
 # Fungsi untuk mengirim notifikasi ke Telegram
-def send_notification_telegram(software_name, mungki_version, web_version):
+def send_notification_telegram(software_name, Munki_version, web_version):
     telegram_token = "8184924708:AAGZ56uxf7LzbukNx2tdx-F148-9NtLdhOM"  # Ganti dengan token bot Telegram kamu
     chat_id = "-4523501737"  # Ganti dengan chat ID yang sesuai
     if not telegram_token or not chat_id:
         print("Telegram token atau chat ID belum diset.")
         return
     
-    telegram_message = f"Update Available for {software_name}!\nMungki version: {mungki_version}\nLatest version: {web_version}"
+    telegram_message = f"Update Available for {software_name}!\nMunki version: {Munki_version}\nLatest version: {web_version}"
     send_text_url = f"https://api.telegram.org/bot{telegram_token}/sendMessage"
     params = {
         'chat_id': chat_id,
@@ -97,14 +97,14 @@ def main():
     latest_webex_version_mac = check_latest_version_webex_mac()
     
     if latest_webex_version_mac:
-        webex_mungki_version, webex_web_version = versions.get('Webex Mac', (None, None))
+        webex_Munki_version, webex_web_version = versions.get('Webex Mac', (None, None))
 
-        if compare_versions(webex_mungki_version, latest_webex_version_mac):
-            print(f"Versi baru Webex untuk Mac tersedia: {latest_webex_version_mac}")
+        if compare_versions(webex_Munki_version, latest_webex_version_mac):
+            print(f"Version baru Webex untuk Mac tersedia: {latest_webex_version_mac}")
             update_web_version_csv("Webex Mac", latest_webex_version_mac)
-            send_notification_telegram("Webex Mac", webex_mungki_version, latest_webex_version_mac)
+            send_notification_telegram("Webex Mac", webex_Munki_version, latest_webex_version_mac)
         else:
-            print("Versi Webex untuk Mac sudah yang terbaru.")
+            print("Version Webex untuk Mac sudah yang terbaru.")
     else:
         print("Tidak dapat mengambil versi terbaru untuk Mac.")
 
