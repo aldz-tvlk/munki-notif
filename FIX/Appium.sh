c0215aa457c9ce2269608abe00c13ef842be04f3
KhanhaKP <it-khanha.prasatya@traveloka.com>
2025-05-28 11:43:54 +0700

    Update Appium.sh

--- FIX/Appium.sh
+++ FIX/Appium.sh
@@ -8,7 +8,7 @@ def check_latest_version_appium():
     url = "https://api.github.com/repos/appium/appium-inspector/releases/latest"
     headers = {
         "Accept": "application/vnd.github+json",
-        "Authorization": "Bearer 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"  # Ganti dengan token GitHub kamu
+        "Authorization": "Bearer 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"  # Ganti dengan token GitHub kamu
     }
 
     response = requests.get(url, headers=headers)
